1740e013b0776777027898e3928c497ede1bb9d5
Tomasz Dutkiewicz <tomasz.dutkiewicz@aat.pl>
2022-08-01 13:26:51 +0200

    working

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,20 @@
 import React from "react";
 import Square from "./Square";
+import { calculateWinner } from "./../utils/utils";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { squares: Array(9).fill(null) };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const status = "Next player: x";
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
